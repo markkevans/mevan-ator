2416c2a4f5fdfdf483295012a81653b3e06955ae
Mark Evans <mevans1016@NME-12516M.local>
2012-10-11 14:51:48 +0100

    Tidyup API

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ run = function(client) {
   var app = express();
 
   app.engine('.ejs', require('ejs').__express);
-  
-  //app.register( '.ejs', require('ejs') );
   app.use( express.static(__dirname + '/public') );
   app.use( express.bodyParser() );
   app.use( express.cookieParser() );
@@ -16,30 +14,48 @@ run = function(client) {
     res.render('error.ejs', { err: err });
   });
   
-  var data = new mongodb.Collection(client, 'data');
+  var requests = new mongodb.Collection(client, 'requests');
+  var responses = new mongodb.Collection(client, 'responses');
 
   app.get('/',function(req,res){
       res.header('Cache-Control','private');
       res.render('index.ejs');
   });
 
-  app.post('/',function(req,res){
-    var to_insert = { name: req.body.name, status: req.body.status, user_id: req.body.user_id};
-    data.insert( to_insert, { safe: true }, function(err,objects) {
+  app.post('/requests',function(req,res){
+    expected = req.body.expected.replace(/(^\s*)|(\s*$)/g,'').replace(/\s{2,}/g,' ').split(' ');
+    var request = { user_id: req.body.name, expected: expected, date_time: new Date()};
+    requests.insert( request, { safe: true }, function(err,objects) {
+      if ( err ) { throw new Error(err); }
+      res.redirect("/");
+    });
+  });
+
+  app.post('/responses',function(req,res){
+    var response = { name: req.body.name, status: req.body.status, user_id: req.body.user_id, date_time: new Date()};
+    responses.insert( response, { safe: true }, function(err,objects) {
       if ( err ) { throw new Error(err); }
       res.redirect("/");
     });
   });
 
   app.get('/list',function(req,res){
-    data.find( {}, {} ).sort({_id:-1}).limit(20).toArray( function(err,docs) {
+    requests.find( {}, {} ).sort({_id:-1}).limit(20).toArray( function(err,docs) {
       if ( err ) { throw new Error(err); }
       res.header('Cache-Control','private');
-      res.render( 'list.ejs', { data: docs } );
+      res.render( 'list.ejs', { requests: docs } );
     });
   });
 
-  var port = 80;
+  app.get('/responses/:id',function(req,res){
+    var id = new client.bson_serializer.ObjectID(req.params.id);
+    responses.findOne( { _id: id }, function(err,doc) {
+      if ( err ) { throw new Error(err); }
+      res.render('responses.ejs', { responses: doc } );
+    });
+  });
+
+  var port = 443;
   app.listen(port);
   console.log('Server listing on port '+ port);
 };
